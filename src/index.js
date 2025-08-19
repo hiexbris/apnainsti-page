8a0fc15bb60354486095fb85cadd191c2e9ace0a
Daksh Yadav <dakshyadav1810@gmail.com>
2025-08-19 16:27:26 +0530

    Upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ApnaInstiPage from "./pages/apnaInsti";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <ApnaInstiPage />
+  </React.StrictMode>
+);
